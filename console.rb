3c508b5ab65d9915c64ff8e13406546178566965
Jáchym Toušek <enumag@gmail.com>
2022-03-25 22:21:15 +0100

    Fix indentation

--- console.rb
+++ console.rb
@@ -23,38 +23,38 @@ $CONFIG = Config.new(config)
 plugin = DataImporterExporter.new
 
 if $COMMAND == "import"
-    plugin.on_start
+  plugin.on_start
 elsif $COMMAND == "export"
-    plugin.on_exit
+  plugin.on_exit
 elsif $COMMAND == "start"
-    require 'listen'
-    require 'wdm'
+  require 'listen'
+  require 'wdm'
 
-    plugin.on_start
+  plugin.on_start
 
-    # Dump the system time at startup into a file to read later
-    dump_startup_time
+  # Dump the system time at startup into a file to read later
+  dump_startup_time
 
-    # Definitely do not want the user to close the command window
-    puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    puts "!!!DO NOT CLOSE THIS COMMAND WINDOW!!!"
-    puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    puts_verbose
+  # Definitely do not want the user to close the command window
+  puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+  puts "!!!DO NOT CLOSE THIS COMMAND WINDOW!!!"
+  puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+  puts_verbose
 
-    listener = Listen.to($PROJECT_DIR + $CONFIG.data_dir) do |modified, added, removed|
-        plugin.on_exit
-    end
-    listener.start
-
-    # Start RMXP
-    File.write($PROJECT_DIR + '/Game.rxproj', 'RPGXP 1.05')
-    system('START /B /WAIT /D"' + $PROJECT_DIR + '" Game.rxproj')
-    File.delete($PROJECT_DIR + '/Game.rxproj')
-
+  listener = Listen.to($PROJECT_DIR + $CONFIG.data_dir) do |modified, added, removed|
     plugin.on_exit
+  end
+  listener.start
 
-    # Delete the startup timestamp
-    load_startup_time(true)
+  # Start RMXP
+  File.write($PROJECT_DIR + '/Game.rxproj', 'RPGXP 1.05')
+  system('START /B /WAIT /D"' + $PROJECT_DIR + '" Game.rxproj')
+  File.delete($PROJECT_DIR + '/Game.rxproj')
+
+  plugin.on_exit
+
+  # Delete the startup timestamp
+  load_startup_time(true)
 else
-    puts "Unknown command " + $COMMAND
+  puts "Unknown command " + $COMMAND
 end
