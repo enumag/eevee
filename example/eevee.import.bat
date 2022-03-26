6d0f0d5d7287400987b63a0fd303db349eab7b4c
Jáchym Toušek <enumag@gmail.com>
2022-03-27 00:27:29 +0100

    Fix bat files

--- example/eevee.import.bat
+++ example/eevee.import.bat
@@ -4,4 +4,4 @@
 :: https://stackoverflow.com/questions/17118846/how-to-prevent-batch-window-from-closing-when-error-occurs
 if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
 
-"./eevee.exe" "import" && sleep 1 && exit
+"./eevee.exe" "import" && timeout /t 1 /nobreak > NUL && exit
--- example/eevee.rmxp.bat
+++ example/eevee.rmxp.bat
@@ -4,4 +4,4 @@
 :: https://stackoverflow.com/questions/17118846/how-to-prevent-batch-window-from-closing-when-error-occurs
 if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
 
-"./eevee.exe" "rmxp" && sleep 1 && exit
+"./eevee.exe" "rmxp" && timeout /t 1 /nobreak > NUL && exit
