79d9311cbfc0a6873b1ee67d6a41b5d43cadd2f6
Jáchym Toušek <enumag@gmail.com>
2024-01-02 16:22:23 +0100

    Improve example files

--- example/eevee.rmxp.bat
+++ example/eevee.rmxp.bat
@@ -1,9 +1,16 @@
 @echo off
 
+:: Hack to minimize the window. Source:
+:: https://stackoverflow.com/questions/2106723/how-to-minimize-the-command-prompt-when-running-a-batch-script
+if not "%minimized%"=="" goto :minimized
+"./eevee.exe" "import"
+set minimized=true
+start /min cmd /C "%~dpnx0"
+goto :EOF
+:minimized
+
 :: Hack to keep the window open on error. Source:
 :: https://stackoverflow.com/questions/17118846/how-to-prevent-batch-window-from-closing-when-error-occurs
 if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
-
-"./eevee.exe" "import"
 
 "./eevee.exe" "rmxp" && timeout /t 1 /nobreak > NUL && exit
